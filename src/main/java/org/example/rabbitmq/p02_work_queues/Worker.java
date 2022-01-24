333a109c8eac4955b211414d4089bccf2d3c517d
AxelNordov <iq@ua.fm>
2022-01-24 12:38:13 +0200

    Work Queues - Message durability

--- src/main/java/org/example/rabbitmq/p02_work_queues/Worker.java
+++ src/main/java/org/example/rabbitmq/p02_work_queues/Worker.java
@@ -6,7 +6,8 @@ import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.DeliverCallback;
 
 public class Worker {
-    private final static String QUEUE_NAME = "hello";
+
+    private final static String TASK_QUEUE_NAME = "task_queue";
 
     public static void main(String[] argv) throws Exception {
         ConnectionFactory factory = new ConnectionFactory();
@@ -14,7 +15,8 @@ public class Worker {
         Connection connection = factory.newConnection();
         Channel channel = connection.createChannel();
 
-        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+        boolean durable = true;
+        channel.queueDeclare(TASK_QUEUE_NAME, durable, false, false, null);
         System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
 
         DeliverCallback deliverCallback = (consumerTag, delivery) -> {
@@ -31,11 +33,12 @@ public class Worker {
             }
         };
         boolean autoAck = false; // acknowledgment is covered below
-        channel.basicConsume(QUEUE_NAME, autoAck, deliverCallback, consumerTag -> { });
+        channel.basicConsume(TASK_QUEUE_NAME, autoAck, deliverCallback, consumerTag -> {
+        });
     }
 
     private static void doWork(String task) throws InterruptedException {
-        for (char ch: task.toCharArray()) {
+        for (char ch : task.toCharArray()) {
             if (ch == '.') Thread.sleep(1000);
         }
     }
